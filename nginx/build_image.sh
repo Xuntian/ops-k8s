f4248158ffee7a189bf2e7541160ccecf9fd8865
Xuntian <li.zq@foxmail.com>
2018-05-11 15:37:36 +0800

    cicd for nginx

---
+++ nginx/build_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PWD=$(pwd)
+PATH_DOCKERFILE=$PWD/Dockerfile
+IMAGE_TAG="v1"
+
+if [[ ! -e $PATH_DOCKERFILE ]]; then 
+    echo "$PATH_DOCKERFILE not found"
+    exit 1
+else 
+    docker build -t keepwork/stage/nginx:$IMAGE_TAG .
+fi
+
+REGISTRY_HOST="10.28.18.13"
+docker tag keepwork/stage/nginx:$IMAGE_TAG $REGISTRY_HOST/keepwork/stage/nginx:$IMAGE_TAG
+docker pus $REGISTRY_HOST/keepwork/stage/nginx:$IMAGE_TAG
