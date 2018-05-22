49d8111409e5c87408dd5c5a340aa0aac39cf9d0
Xuntian <li.zq@foxmail.com>
2018-05-22 19:17:51 +0800

    add keepwork.yaml

---
+++ node/env.dockerfile
@@ -0,0 +1,5 @@
+FROM node:10
+MAINTAINER xuntian li.zq@foxmail.com
+
+RUN mkdir /code
+WORKDIR /code
---
+++ node/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -xe
+PWD=$(pwd)
+ENV_DOCKERFILE_PATH=$PWD/env.dockerfile
+if [[ ! -e $ENV_DOCKERFILE_PATH ]]; then 
+    echo "env.dockerfile文件不存在"
+    exit 1
+fi
+docker build -t xuntian/node:10.1-npm-5.6-yarn-1.6 -f env.dockerfile ./
+docker run --rm -v /var/jenkins_home/workspace/node/node:/code xuntian/node:10.1-npm-5.6-yarn-1.6 npm install
+
+
+BUILD_DOCKERFILE_PATH=$PWD/Dockerfile
+if [[ ! -e $BUILD_DOCKERFILE_PATH ]]; then 
+    echo "build.dockerfile文件不存在"
+    exit 1
+fi 
+docker build -t xuntian/node_project:v1 ./
+
+docker tag xuntian/node_project:v1 10.28.18.13/node_project:v1
+docker push 10.28.18.13/node_project:v1
+
+kubectl create -f keepwork.yaml
+
